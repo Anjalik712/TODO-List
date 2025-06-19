3237ab4e41a70e508ce44e2edec45242db4d249e
Anjalik712 <anjali2sheeja@gmail.com>
2025-06-19 12:21:25 +0530

    feat:initial table structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { Table } from "./ui/table";
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, Table],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
