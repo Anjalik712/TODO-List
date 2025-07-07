9775743fc994ad8b06c5f5bffcf4cd853013bdcd
Anjalik712 <anjali2sheeja@gmail.com>
2025-07-07 16:47:28 +0530

    fix:Use routing to display the component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Route } from '@angular/router';
+import { TodoListWrapperComponent } from './todo-list-wrapper/todo-list-wrapper.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'todo-list-wrapper', component: TodoListWrapperComponent },
+  { path: '', redirectTo: '/todo-list-wrapper', pathMatch: 'full' },
+];
