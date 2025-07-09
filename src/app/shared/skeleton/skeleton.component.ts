c8e60d19879e49e7027bb84e94d0a8b85e8460c3
Anjalik <anjali2sheeja@gmail.com>
2025-07-09 12:35:55 +0530

    feat:create shared skeleton component

---
+++ src/app/shared/skeleton/skeleton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-skeleton',
+  imports: [CommonModule],
+  templateUrl: './skeleton.component.html',
+  styleUrl: './skeleton.component.scss',
+})
+export class SkeletonComponent {}
