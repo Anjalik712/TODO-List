cea854e1d21063779769d62b0b13da6c66a08d52
Anjalik712 <anjali2sheeja@gmail.com>
2025-07-10 12:08:28 +0530

    fix: use root for todo list wrapper

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,5 @@ import { Route } from '@angular/router';
 import { TodoListWrapperComponent } from './todo-list-wrapper/todo-list-wrapper.component';
 
 export const appRoutes: Route[] = [
-  { path: 'todo-list-wrapper', component: TodoListWrapperComponent },
-  { path: '', redirectTo: '/todo-list-wrapper', pathMatch: 'full' },
+  { path: '', component: TodoListWrapperComponent },
 ];
